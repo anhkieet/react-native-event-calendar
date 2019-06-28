ec880978de8790936237cd50b2b981c1ddd76533
David Bui <kiet.bui@theappteam.com.au>
2019-06-28 20:26:00 +0700

    fix bug

--- src/EventCalendar.js
+++ src/EventCalendar.js
@@ -30,8 +30,8 @@ export default class EventCalendar extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.start !== this.props.start || nextProps.end !== this.props.end) {
-      const start = props.start ? props.start : 0;
-      const end = props.end ? props.end : 24;
+      const start = this.props.start ? this.props.start : 0;
+      const end = this.props.end ? this.props.end : 24;
       this.styles = styleConstructor(props.styles, (end - start) * 100);
     }
   }
